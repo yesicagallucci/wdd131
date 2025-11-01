81fd9fa18159862fce970f1cd955f24ac6cb9786
Yesica Gallucci <121723257+yesicagallucci@users.noreply.github.com>
2025-11-01 20:38:14 -0300

    udir retouches

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 let lastModified = document.lastModified;
-document.getElementById("lastModified").innerHTML = "Last Modification: " + lastModified;
+document.getElementById("lastModified").innerHTML = "Last modification: " + lastModified;
 
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
